bc50f26fe88ebb6eab7a95fa86947e7dad6aa184
yash chauhan <yashchauhan.gaya@gmail.com>
2025-03-01 11:42:09 +0530

    Initial commit for LearnKafka project

---
+++ user-service/user-service/src/main/java/com/company/learnKafka/user_service/Entity/User.java
@@ -0,0 +1,22 @@
+package com.company.learnKafka.user_service.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+}
---
+++ user-service/user-service/src/main/java/com/company/learnKafka/user_service/service/UserService.java
@@ -0,0 +1,34 @@
+package com.company.learnKafka.user_service.service;
+
+
+import com.company.learnKafka.user_service.Entity.User;
+import com.company.learnKafka.user_service.dto.CreateUserRequestDto;
+import com.company.learnKafka.user_service.event.UserCreatedEvent;
+import com.company.learnKafka.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    @Value("${kafka.topic.user-created-topic}")
+    private String KAFKA_USER_CREATED_TOPIC;
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final KafkaTemplate<Long, UserCreatedEvent> kafkaTemplate;
+
+    public void createUser(CreateUserRequestDto createUserRequestDto){
+        User user = modelMapper.map(createUserRequestDto,User.class);
+      User savedUser =  userRepository.save(user);
+
+        UserCreatedEvent userCreatedEvent = modelMapper.map(savedUser, UserCreatedEvent.class);
+        kafkaTemplate.send(KAFKA_USER_CREATED_TOPIC, (userCreatedEvent.getId()),userCreatedEvent);
+
+
+    }
+}
